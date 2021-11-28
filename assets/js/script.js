ede29e875e7b42f83372f668ddeb6cc20f140dff
ncp9988@gmail.com <v.i.c@Chaus-MBP.hitronhub.home>
2021-11-28 12:24:06 -0600

    Changed logic to place API request only after user clicks search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,33 @@
 var cityInputEl = document.querySelector("#cityname")
 var weatherContainer = document.querySelector("#weather-container")
 var apiKey = "b76284fed56f40aa553158bccc226955"
+var btn = document.querySelector("#btn")
+var saveCity = document.querySelector("#save-city")
 
-var nameSumitHandler = function (event) {
+
+
+
+var getCityWeather = function (city) {
+    //format OpenWeather One Call API
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b76284fed56f40aa553158bccc226955";
+    console.log(apiUrl)
+
+    // make a get request to url
+    fetch(apiUrl)
+        .then(function (response) {
+            // request was successful
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    displayCity(data, city);
+                });
+            };
+        })
+};
+var nameSumitHandler = function(event) {
     event.preventDefault();
-    var cityname = cityInputEl.value.trim();
-
+    var cityname = cityInputEl.value;
     if (cityname) {
         getCityWeather(cityname);
 
@@ -16,25 +38,32 @@ var nameSumitHandler = function (event) {
         alert("Please enter a City");
     }
 }
-// var getCityWeather = function(city) {
-//     //format OpenWeather One Call API
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}"
 
-//     // make a get request to url
-//     fetch(apiUrl)
-//         .then(function(response) {
-//             // request was successful
-//             if (response.ok) {
-//                 console.log(response);
-//                 response.json().then(function (data) {
-//                     console.log(data);
-//                     displayCity(data, city);
-//                 });
-//             };
-//         })
-// };
+btn.addEventListener("click", nameSumitHandler);
 
-var getCityWeather =function() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}`)
-};
-getCityWeather();
+
+
+var displayCity = function(city) {
+
+    console.log(city)
+    //check api
+    if (city.length === 0) {
+        weatherContainer.textContent = "No city found.";
+        return;
+    }
+    weatherContainer.textContent = city;
+
+    //loop over citys
+    for (var i = 0; i < city.length; i++) {
+        var citynameEl = city[i].main.name
+
+        
+        var titleEl = document.createElement("div");
+        titleEl.textContent = citynameEl;
+
+        //append to container
+        citynameEl.appendChild(saveCity)
+        
+    }
+
+}
