f38db3f41cfca9dc3dc792241065da1940243998
ncp9988@gmail.com <v.i.c@Chaus-MBP.hitronhub.home>
2021-11-28 13:32:24 -0600

    Save previous search to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var weatherContainer = document.querySelector("#weather-container")
 var apiKey = "b76284fed56f40aa553158bccc226955"
 var btn = document.querySelector("#btn")
 var saveCity = document.querySelector("#save-city")
-
+var previousSearch = JSON.parse(localStorage.getItem("WeatherDashboard")) || []
 
 
 var getCityWeather = function (city) {
@@ -12,24 +12,27 @@ var getCityWeather = function (city) {
     console.log(apiUrl)
 
     // make a get request to url
-    fetch(apiUrl)
-        .then(function (response) {
-            // request was successful
-            if (response.ok) {
-                console.log(response);
-                response.json().then(function (data) {
-                    console.log(data);
-                    var lat = data.coord.lat
-                    var lon = data.coord.lon
-                    forecast(lat, lon);
-                    weatherContainer.innerHTML = `<h3>City: ${city}</h3>
-                    <h6>Description:${data.weather[0].description}<span><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></span></h6>
-                    <p>Humidity:${data.main.humidity}</p>
-                    <p>Wind speed:${data.wind.speed}</p>
-                    `
-                });
-            };
-        })
+        fetch(apiUrl)
+            .then(function (response) {
+                // request was successful
+                if (response.ok) {
+                    console.log (response);
+                    response.json().then(function (data) {
+                        console.log(data);
+                        var lat = data.coord.lat
+                        var lon = data.coord.lon
+                        forecast(lat, lon);
+                        weatherContainer.innerHTML = `<h3>City: ${city}</h3>
+                        <h6>Description:${data.weather[0].description}<span><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></span></h6>
+                        <p>Humidity:${data.main.humidity}</p>
+                        <p>Wind speed:${data.wind.speed}</p>
+                        `
+                        previousSearch.push(city)
+                        localStorage.setItem("WeatherDashboard", JSON.stringify(previousSearch))
+                        displayCity()
+                    });
+                };
+            })
 };
 var nameSumitHandler = function (event) {
     event.preventDefault();
@@ -49,46 +52,50 @@ btn.addEventListener("click", nameSumitHandler);
 
 
 
-var displayCity = function (city) {
+var displayCity = function () {
+    let city = JSON.parse(localStorage.getItem("WeatherDashboard")) || []
 
-    console.log(city)
+
+
     //check api
     if (city.length === 0) {
-        weatherContainer.textContent = "No city found.";
-        return;
-    }
-    weatherContainer.textContent = city;
-
-    //loop over citys
-    for (var i = 0; i < city.length; i++) {
-        var citynameEl = city[i].main.name
-
-
-        var titleEl = document.createElement("div");
-        titleEl.textContent = citynameEl;
-
-        //append to container
-        citynameEl.appendChild(saveCity)
+        saveCity.textContent = "No city found.";
 
     }
+    else {
+        saveCity.innerHTML = ``
 
-}
+        //loop over citys
+        for (var i = 0; i < city.length; i++) {
+            var citynameEl = city[i]
 
-var forecast = function (lat, lon) {
-    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
-    console.log(apiUrl)
 
-    // make a get request to url
-    fetch(apiUrl)
-        .then(function (response) {
-            // request was successful
-            if (response.ok) {
-                console.log(response);
-                response.json().then(function (data) {
-                    console.log(data);
-                    var daily = data.daily
-                    for (let i = 1; i <= 5; i++) {
-                        document.getElementById(`${i}`).innerHTML = `<div>
+            var titleEl = document.createElement("div");
+            titleEl.textContent = citynameEl;
+
+            //append to container
+            saveCity.appendChild(titleEl)
+
+        }
+    }
+
+    }
+
+    var forecast = function (lat, lon) {
+        var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+        console.log(apiUrl)
+
+        // make a get request to url
+        fetch(apiUrl)
+            .then(function (response) {
+                // request was successful
+                if (response.ok) {
+                    console.log(response);
+                    response.json().then(function (data) {
+                        console.log(data);
+                        var daily = data.daily
+                        for (let i = 1; i <= 5; i++) {
+                            document.getElementById(`${i}`).innerHTML = `<div>
                         <h3>Day ${i}</h3>
                         <h4>Temp:${daily[i].temp.day}</h4>
                         <h6>Description:${daily[i].weather[0].description}<span><img src="https://openweathermap.org/img/wn/${daily[i].weather[0].icon}@2x.png" /></span></h6>
@@ -96,10 +103,12 @@ var forecast = function (lat, lon) {
                     <p>Wind speed:${daily[i].wind_speed}</p>
                       </div>
                         `
-                    }
-                })
-            }
-        })
+                        }
+                    })
+                }
+            })
 
-}
+    }
 
+
+    displayCity()
